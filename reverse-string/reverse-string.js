5c9f856799deb50ec91745d83e6e19c998ac2159
Robert Rossol <shyguyrbr1@gmail.com>
2018-01-23 23:58:41 -0600

    reverseString started, string broken into array and reversed

---
+++ reverse-string/reverse-string.js
@@ -0,0 +1,13 @@
+
+var reverseString = function (input) {
+  this.input = input
+  // return this.input
+  var final = []
+  var broken = this.input.split('')
+  while (broken.length > 0) {
+    final.push(broken.splice(-1))
+  }
+  return final
+};
+
+module.exports = reverseString;
